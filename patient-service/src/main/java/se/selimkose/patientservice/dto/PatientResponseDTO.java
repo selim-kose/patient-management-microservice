9c0fae0f6298a480de7f34ba1c0f1e32de886b3f
selim <selim-kose@live.com>
2025-06-09 09:43:40 +0200

    Add PatientResponeDTO

---
+++ patient-service/src/main/java/se/selimkose/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,12 @@
+package se.selimkose.patientservice.dto;
+
+import lombok.Data;
+
+@Data
+public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+}
