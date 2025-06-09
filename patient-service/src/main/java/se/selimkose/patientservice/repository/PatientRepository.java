7406a9d047ff6097efe79ce0f3d695ef44c3f915
selim <selim-kose@live.com>
2025-06-09 09:31:37 +0200

    Add patient repository

---
+++ patient-service/src/main/java/se/selimkose/patientservice/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package se.selimkose.patientservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.selimkose.patientservice.model.Patient;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}
