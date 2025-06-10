739ed49ca63650c928459f84739c21abbb860052
selim <selim-kose@live.com>
2025-06-10 14:13:06 +0200

    Add PatientResponseDTO

---
+++ patient-service/src/main/java/se/selimkose/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,21 @@
+package se.selimkose.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PatientRequestDTO {
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 100, message = "Name can not be longer than 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Registered date is required")
+    private String registeredDate;
+}
