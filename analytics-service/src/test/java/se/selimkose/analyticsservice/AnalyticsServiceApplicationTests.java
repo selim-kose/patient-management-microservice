250ea193ade846ce4c06c28f641284f338ec1089
selim <selim-kose@live.com>
2025-06-16 12:32:11 +0200

    Add analytics-service

---
+++ analytics-service/src/test/java/se/selimkose/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.selimkose.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
