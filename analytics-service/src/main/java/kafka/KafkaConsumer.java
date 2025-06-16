0e2d512d6ed112a57245ac6988c934388f46ef00
selim <selim-kose@live.com>
2025-06-16 12:46:14 +0200

    Add KafkaComsumer

---
+++ analytics-service/src/main/java/kafka/KafkaConsumer.java
@@ -0,0 +1,27 @@
+package kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer {
+
+    private final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "patient-service", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            //TODO business logic
+            log.info("Received PatientEvent: {}", patientEvent);
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Failed to parse PatientEvent from byte array", e.getMessage());
+        }
+
+    }
+}
