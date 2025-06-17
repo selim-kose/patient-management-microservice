66b03a92cc486eccabb827f7732afecd3ffc0dca
selim <selim-kose@live.com>
2025-06-17 18:48:34 +0200

    Add auth-service

---
+++ auth-service/src/main/java/se/selimkose/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package se.selimkose.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
---
+++ auth-service/src/main/java/se/selimkose/authservice/model/User.java
@@ -0,0 +1,21 @@
+package se.selimkose.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+}
