30ff7cddbf06e51b1f1892b2ff674983670b0381
selim <selim-kose@live.com>
2025-06-17 14:21:25 +0200

    Add api-gateway

---
+++ api-gateway/src/test/java/se/selimkose/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package se.selimkose.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
