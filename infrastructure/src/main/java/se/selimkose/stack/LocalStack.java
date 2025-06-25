f2706f14d4bd255e8e685bf57e5d160a5dc61668
selim <selim-kose@live.com>
2025-06-25 07:44:50 +0200

    Add Kafka MSK Cluster

--- infrastructure/src/main/java/se/selimkose/stack/LocalStack.java
+++ infrastructure/src/main/java/se/selimkose/stack/LocalStack.java
@@ -5,6 +5,7 @@ import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
@@ -26,6 +27,8 @@ public class LocalStack extends Stack {
         // Create health checks for the RDS instances
         CfnHealthCheck authServiceDBHealthCheck = createDBHealthCheck("AuthServiceHealthCheck", authServiceDB);
         CfnHealthCheck patientServiceDBHealthCheck = createDBHealthCheck("PatientServiceHealthCheck", patientServiceDB);
+
+        CfnCluster kafkaMSKCluster = createMSKKafkaCluster();
     }
 
     public static void main(String[] args) {
@@ -59,6 +62,14 @@ public class LocalStack extends Stack {
                 .removalPolicy(RemovalPolicy.DESTROY)   // Destroy the database on stack deletion
                 .build();
     }
+    /**
+     * Creates a health check for the given RDS database instance.
+     * This method uses TCP health checks to ensure the database is reachable.
+     *
+     * @param id The identifier for the health check resource.
+     * @param dbName The RDS database instance to create a health check for.
+     * @return A CfnHealthCheck resource configured for the RDS instance.
+     */
 
     private CfnHealthCheck createDBHealthCheck(String id, DatabaseInstance dbName) {
         return CfnHealthCheck.Builder.create(this, id)
@@ -68,6 +79,25 @@ public class LocalStack extends Stack {
                         .ipAddress(dbName.getDbInstanceEndpointAddress()) // Use the endpoint address of the DB instance
                         .requestInterval(30) // Check every 30 seconds
                         .failureThreshold(3) // Fail after 3 consecutive failures
+                        .build())
+                .build();
+    }
+
+    /**
+     * Creates an MSK (Managed Streaming for Kafka) cluster.
+     * This is a placeholder method and should be called if you want to create a Kafka MSK cluster.
+     */
+    private CfnCluster createMSKKafkaCluster() {
+        return CfnCluster.Builder.create(this, "MSKCluster")
+                .clusterName("patient-management-kafka-cluster")
+                .kafkaVersion("2.8.0") // Specify the Kafka version
+                .numberOfBrokerNodes(1) // Use 3 broker nodes for redundancy
+                .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.large") // Use a suitable instance type for brokers
+                        .clientSubnets(this.vpc.getPrivateSubnets().stream()
+                                .map(subnet -> subnet.getSubnetId())
+                                .toList()) // Use private subnets for the brokers
+                        .brokerAzDistribution("DEFAULT") // Distribute brokers across AZs
                         .build())
                 .build();
     }
